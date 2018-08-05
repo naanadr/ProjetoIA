8b9c4c02a77c41452cf78f5e0eab42f50d649892
Fernanda <fsandrade25@gmail.com>
2018-08-05 01:07:21 -0300

    adicionei o main.py utiliza o vetor de imagens, não o HOG. Editei o perceptron (que não funciona, por causa do HOG)

--- Perceptron.py
+++ Perceptron.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 # encoding: iso-8859-1
 # encoding: win-1252
+
 import cv2
 import os
 import imutils
@@ -56,70 +57,68 @@ Variáveis necessárias:
 # Perceptron com apenas uma camada
 class Perceptron:
 	# incializa o perceptron
-	def __init__(self, epocas, taxa_aprendizagem, entradas, saidas, limiar):
-		self.entradas = entradas            # entradas para teste
-		self.saidas = saidas                # suas saidas esperadas para cada entrada
+	def __init__(self, epocas, taxa_aprendizagem):
+		self.entradas = []            # entradas para teste
+		self.saidas = []              # suas saidas esperadas para cada entrada
 		self.epocas = epocas
 		self.taxa_aprendizagem = taxa_aprendizagem
 		self.pesos = []                         # vetor com os pesos que serão atribuidos a cada entrada
-		self.total_entradas = len(entradas)     # quantidade total de entradas que serão testadas
-		self.total_variaveis_entrada = len(entradas[0])
-		self.limiar = limiar
+		self.total_entradas = 0     # quantidade total de entradas que serão testadas
+		self.total_variaveis_entrada = 0
+		self.arquivo = ""
 
-	def treinar(self):
-		# adiciona -1 para cada uma das amostras
-		#for amostra in self.entradas:
-		#	amostra.insert(0, -1)
+	def preencher_vetor(self):
 		# onde cada posição armazena um vetor com os pesos iniciais
-		for i in range(self.total_variaveis_entrada):
+		for i in range(648):
 			# preencher o vetor que armazena os pesos
 			self.pesos.append(np.random.random_sample())
 
-		# insere o limiar no vetor de pesos
-		#self.pesos.insert(0, self.limiar)
-
+	def treinar(self):
 		# roda o algoritmo ate terminar as eras estabelecidas
 		for i in range(self.epocas):
 			erro = False
 			# repetir o processo a quantidade total de entradas
 			for j in range(self.total_entradas):
-				somatorio = 0
-				# somar os pesos com as variaveis da entrada
-				for posicao in range(self.total_variaveis_entrada):
-					somatorio += self.pesos[posicao] * self.entradas[j][posicao]
-
-				# função de ativação
-				saida = self.ativacao(somatorio)
-
-				if saida != self.saidas[j]:
-					erro = True
-					erro_auxiliar = self.saidas[j] - saida
-
-					# corrigir o peso agora para cada entrada dessa rodada
+				if len(self.entradas[j]) == 648:
+					somatorio = 0
+					# somar os pesos com as variaveis da entrada
 					for posicao in range(self.total_variaveis_entrada):
-						self.pesos[posicao] = self.pesos[posicao] + \
-											  (self.taxa_aprendizagem * erro_auxiliar * self.entradas[j][posicao])
+						somatorio += self.pesos[posicao] * self.entradas[j][posicao]
 
+					# função de ativação
+					saida = self.ativacao(somatorio)
+
+					if saida != self.saidas[j]:
+						erro = True
+						erro_auxiliar = self.saidas[j] - saida
+
+						# corrigir o peso agora para cada entrada dessa rodada
+						for posicao in range(self.total_variaveis_entrada):
+							self.pesos[posicao] = self.pesos[posicao] + \
+												  (self.taxa_aprendizagem * erro_auxiliar * self.entradas[j][posicao])
+				else:
+					print 'não possui 648 caracteristicas'
 			# se achar os melhores valores antes do fim das eras
 			if not erro:
+				print 'não encontrou mais erro'
 				break
 
 	def testar(self):
 		resposta_esperada = []
 		amostras = []
-		arquivo = teste
-		local = os.listdir(arquivo)
+		local = os.listdir(self.arquivo)
 		cont = 0
+
+		hogControlador = hog_gerador()
+
 		for im_file in local:
 			vetor_caracteristica_aux = []
 
-			im_path = arquivo + im_file
+			im_path = self.arquivo + im_file
 
 			im = cv2.imread(im_path)
-			resized_image = imutils.resize(im, width=80, height=80)
 
-			fd, hog_image = hog(resized_image, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(3, 3),
-			                    visualize=True, feature_vector=True)
+			fd, hog_image = hogControlador.hog_utilizar(im)
 
 			for i in range(len(fd)):
 				teste = float(fd[i])
@@ -155,10 +154,20 @@ class Perceptron:
 		return 1 if valor >= 0 else 0
 
 class hog_gerador:
-	def __init__(self):
+	def __init__(self, arquivos_treino):
 		self.vetor_caracteristica = []
 		self.saidas_esperadas = []
-		self.arquivos = treinar
+		self.arquivos = arquivos_treino + '/'
+
+	def hog_utilizar(self, im):
+		resized_image = imutils.resize(im, width=80, height=80)
+
+		fd, hog_image = hog(resized_image, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(3, 3),
+		                    visualize=True, feature_vector=True)
+
+		print 'aqui ', fd
+
+		return fd, hog_image
 
 	def gerar_vetor(self):
 		local = os.listdir(self.arquivos)
@@ -170,10 +179,8 @@ class hog_gerador:
 			im_path = self.arquivos + im_file
 
 			im = cv2.imread(im_path)
-			resized_image = imutils.resize(im, width=80, height=80)
 
-			fd, hog_image = hog(resized_image, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(3,3),
-									  visualize=True, feature_vector=True)
+			fd, hog_image = self.hog_utilizar(im)
 
 			for i in range(len(fd)):
 				teste = float(fd[i])
@@ -205,29 +212,53 @@ class hog_gerador:
 
 # ---------------------------------------------------------------------------------------------------
 # MAIN:
-accuracy_array = list()
-for i in range(5):
-	teste = "string_path"+str(i)             #Separar para Teste
-	for n in range(5):
-		if not n == i:
-            treino = "string_path"+str(n)    #Treino da vez
 
-			hogControlador = hog_gerador()
+# Local com os arquivos:
+string_path = "5_fold/"
+accuracy_array = list()
+
+# Posição do folder para teste:
+for i in range(5):
+	# Local do folder de teste:
+	teste = string_path + str(i) + "fold"            #Separar para Teste
+	print 'Teste sendo realizado no: ', teste
+
+	# Instanciar a classe perceptron:
+	perceptronControlador = Perceptron(100, 0.1)
+	# Inicia os pesos com valores aleatorios, para o primeiro teste
+	perceptronControlador.preencher_vetor()
+	# Folder que serão utilizados para treinamento:
+	for n in range(5):
+		# Do folder 0 ate 4, ele treina com todas os folders, menos com o folder que é ele mesmo.
+		if not n == i:
+			# Diretorio com os arquivos de treinamento
+			treino = string_path + str(n) + "fold"    #Treino da vez
+			print 'treino: ', treino
+
+			# Instanciar a classe do hog
+			hogControlador = hog_gerador(treino)
 			print 'gerar vetor...'
+			# Gerar o vetor de caracteristica:
 			hogControlador.gerar_vetor()
 
 			entradas = []
 			saidas = []
 			print 'retirar_informacoes...'
+			# Separar os dados que compoem o vetor de caracteristica
 			entradas, saidas = hogControlador.retirar_informacoes()
-
-			# variando eras e taxa de aprendizado
-			perceptronControlador = Perceptron(100, 0.1, entradas, saidas, -1)
+			# Insere os dados obtidos anteriormente:
+			perceptronControlador.entradas = entradas
+			perceptronControlador.total_entradas = len(entradas)
+			perceptronControlador.total_variaveis_entrada = len(entradas[0])
+			perceptronControlador.saidas = saidas
 
 			print 'treinar...'
+			# Inicia o processo de treinamento:
 			perceptronControlador.treinar()
-            
-    print 'testar...'
+
+	print 'testar...'
+	# Passa o diretorio que está os dados de teste:
+	perceptronControlador.arquivo = teste
 	resposta_obtida, resposta_esperada = perceptronControlador.testar()
 
 	similar = 0
@@ -240,11 +271,12 @@ for i in range(5):
 
 	print 'acertados ', similar
 	print 'total ', total
-	print 'acuracia',accuracy
-	#200 caracteristicas no hog
-	#200 pesos
-	#ativacao bipolar
-for i in accuracy_array:
+	print 'acuracia', accuracy
+	print '\n'
+
+soma = sum(accuracy_array)
+result = soma / 5
+print 'resultado final ', result
+'''for i in accuracy_array:
 	print i
-	soma = soma + i
-result = soma /5
+	soma = soma + i'''
---
+++ main.py
@@ -0,0 +1,187 @@
+# encoding: utf-8
+# encoding: iso-8859-1
+# encoding: win-1252
+
+import cv2
+import os
+import imutils
+from skimage.feature import hog
+import numpy as np
+import random
+
+class Perceptron:
+	# incializa o perceptron
+	def __init__(self, epocas, taxa_aprendizagem):
+		self.entradas = []            # entradas para teste
+		self.saidas = []              # suas saidas esperadas para cada entrada
+		self.epocas = epocas
+		self.taxa_aprendizagem = taxa_aprendizagem
+		self.pesos = []                         # vetor com os pesos que serão atribuidos a cada entrada
+		self.total_entradas = 0     # quantidade total de entradas que serão testadas
+		self.total_variaveis_entrada = 4900     # são 80 vetores com 80 dados em cada um, no caso, 80 * 80 = 4900
+		self.arquivo = ""
+
+	def preencher_vetor(self):
+		# onde cada posição armazena um vetor com os pesos iniciais
+		for i in range(4900):           # serão 4900 dados dentro de cada vetor
+			# preencher o vetor que armazena os pesos
+			self.pesos.append(np.random.random_sample())
+
+	def treinar(self):
+		# roda o algoritmo ate terminar as eras estabelecidas
+		for i in range(self.epocas):
+			erro = False
+			# repetir o processo a quantidade total de entradas
+			for j in range(self.total_entradas):
+				somatorio = 0
+				# somar os pesos com as variaveis da entrada
+				for posicao in range(self.total_variaveis_entrada):
+					somatorio += self.pesos[posicao] * self.entradas[j][posicao]
+
+				# função de ativação
+				saida = self.ativacao(somatorio)
+
+				if saida != self.saidas[j]:
+					erro = True
+					erro_auxiliar = self.saidas[j] - saida
+
+					# corrigir o peso agora para cada entrada dessa rodada
+					for posicao in range(self.total_variaveis_entrada):
+						self.pesos[posicao] = self.pesos[posicao] + \
+											  (self.taxa_aprendizagem * erro_auxiliar * self.entradas[j][posicao])
+			# se achar os melhores valores antes do fim das eras
+			if not erro:
+				break
+
+	def testar(self):
+		descritorControlador = Descritor(self.arquivo)
+
+		amostras, resposta_esperada = descritorControlador.gerar_vetor()
+		resposta = []
+		for amostra in amostras:
+			# utiliza o vetor de pesos que foi ajustado na fase de treinamento
+			u = 0
+			for i in range(self.total_variaveis_entrada):
+				u += self.pesos[i] * amostra[i]
+
+			# calcula a saída da rede
+			y = self.ativacao(u)
+
+			resposta.append(y)
+
+		return resposta, resposta_esperada
+
+	# função de ativação do tipo bipolar (bipolar pq so tem dois valores de saida)
+	# +1 ativo e 0 inativo, com 0 como limiar
+	def ativacao(self, valor):
+		return 1 if valor >= 0 else 0
+
+class Descritor:
+	def __init__(self, arquivo):
+		self.vetor_caracteristica = []
+		self.saidas_esperadas = []
+		self.arquivo = arquivo
+
+	# gera o vetor de caracteristica
+	def gerar_vetor(self):
+		local = os.listdir(self.arquivo)
+
+		for im_file in local:
+			im_path = self.arquivo + '/' + im_file
+			im = cv2.imread(im_path)
+
+			# redimensiona a imagem
+			im = imutils.resize(im, width=80, height=80)
+
+			# converte a imagem em um vetor
+			vetor_cara = np.asarray(im, dtype='uint8')
+
+			# o resultador é um vetor de 80 posicoes, onde cada posição é um outro vetor com 80 valores
+			# concatena todos os vetores em um vetor só
+			vetor_aux = np.array(vetor_cara).ravel()
+
+			string_aux = im_file.split('.')[0]
+			if 'nod' == string_aux:
+				cont = 1
+			elif 'nnod' == string_aux:
+				cont = 0
+
+			self.saidas_esperadas.append(cont)
+			self.vetor_caracteristica.append(vetor_aux)
+
+		return self.vetor_caracteristica, self.saidas_esperadas
+
+# ---------------------------------------------------------------------------------------------------
+# MAIN:
+
+# Local com os arquivos:
+string_path = "5_fold/"
+accuracy_array = list()
+
+# Posição do folder para teste:
+for i in range(5):
+	# Local do folder de teste:
+	teste = string_path + str(i) + "fold"            #Separar para Teste
+	print 'Teste sendo realizado no: ', teste
+
+	# Instanciar a classe perceptron:
+	perceptronControlador = Perceptron(100, 0.1)
+	# Inicia os pesos com valores aleatorios, para o primeiro teste
+	perceptronControlador.preencher_vetor()
+	# Folder que serão utilizados para treinamento:
+	for n in range(5):
+		# Do folder 0 ate 4, ele treina com todas os folders, menos com o folder que é ele mesmo.
+		if not n == i:
+			# Diretorio com os arquivos de treinamento
+			treino = string_path + str(n) + "fold"    #Treino da vez
+			print 'treino: ', treino
+
+			descritorTreinamento = Descritor(treino)
+
+			print 'gerar vetor | retirar_informacoes...'
+			# Separar os dados que compoem o vetor de caracteristica
+			entradas, saidas = descritorTreinamento.gerar_vetor()
+
+			# Insere os dados obtidos anteriormente:
+			perceptronControlador.entradas = entradas
+			perceptronControlador.total_entradas = len(entradas)
+			perceptronControlador.saidas = saidas
+
+			print 'treinar...'
+			# Inicia o processo de treinamento:
+			perceptronControlador.treinar()
+
+			print 'pesos... '
+			print perceptronControlador.pesos
+
+	print 'testar...'
+	# Passa o diretorio que está os dados de teste:
+	perceptronControlador.arquivo = teste
+	resposta_obtida, resposta_esperada = perceptronControlador.testar()
+
+	similar = 0
+	print 'somando...'
+	for i in range(len(resposta_obtida)):
+		if resposta_obtida[i] == resposta_esperada[i]: similar += 1
+	total = len(resposta_obtida)
+	accuracy = float(similar) / float(total)
+	accuracy_array.append(accuracy)
+
+	print 'acertados ', similar
+	print 'total ', total
+	print 'acuracia', accuracy
+	print '\n'
+
+soma = sum(accuracy_array)
+result = soma / 5
+print 'resultado final ', result
+
+
+
+'''Informações para o relatorio:
+as figuras tem dimensões diferentes que variam de 64-65 (h e w) ou 60-65....
+- tamanho novo 80-80 
+
+gerar um vetor com as informações das imagens
+serão 80 vetores, com 80 informações em cada um
+'''
