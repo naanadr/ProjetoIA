47a0985b465469f7628862bc67f268f6c5ed9b04
Fernanda <fsandrade25@gmail.com>
2018-08-06 09:02:36 -0300

    main finalizado

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ class Perceptron:
 		self.taxa_aprendizagem = taxa_aprendizagem
 		self.pesos = []                         # vetor com os pesos que serão atribuidos a cada entrada
 		self.total_entradas = 0     # quantidade total de entradas que serão testadas
-		self.total_variaveis_entrada = 4900     # são 80 vetores com 80 dados em cada um, no caso, 80 * 80 = 4900
+		self.total_variaveis_entrada = 4225     # são 65 vetores com 65 dados em cada um, no caso, 65 * 65 = 4225
 		self.arquivo = ""
 
 	def preencher_vetor(self):
 		# onde cada posição armazena um vetor com os pesos iniciais
-		for i in range(4900):           # serão 4900 dados dentro de cada vetor
+		for i in range(4225):           # serão 4900 dados dentro de cada vetor
 			# preencher o vetor que armazena os pesos
 			self.pesos.append(np.random.random_sample())
 
@@ -41,22 +41,23 @@ class Perceptron:
 			erro = False
 			# repetir o processo a quantidade total de entradas
 			for j in range(self.total_entradas):
-				somatorio = 0
-				# somar os pesos com as variaveis da entrada
-				for posicao in range(self.total_variaveis_entrada):
-					somatorio += self.pesos[posicao] * self.entradas[j][posicao]
-
-				# função de ativação
-				saida = self.ativacao(somatorio)
-
-				if saida != self.saidas[j]:
-					erro = True
-					erro_auxiliar = self.saidas[j] - saida
-
-					# corrigir o peso agora para cada entrada dessa rodada
+				if len(self.entradas[j]) == 4225:
+					somatorio = 0
+					# somar os pesos com as variaveis da entrada
 					for posicao in range(self.total_variaveis_entrada):
-						self.pesos[posicao] = self.pesos[posicao] + \
-											  (self.taxa_aprendizagem * erro_auxiliar * self.entradas[j][posicao])
+						somatorio += self.pesos[posicao] * self.entradas[j][posicao]
+
+					# função de ativação
+					saida = self.ativacao(somatorio)
+
+					if saida != self.saidas[j]:
+						erro = True
+						erro_auxiliar = self.saidas[j] - saida
+
+						# corrigir o peso agora para cada entrada dessa rodada
+						for posicao in range(self.total_variaveis_entrada):
+							self.pesos[posicao] = self.pesos[posicao] + \
+												  (self.taxa_aprendizagem * erro_auxiliar * self.entradas[j][posicao])
 			# se achar os melhores valores antes do fim das eras
 			if not erro:
 				print 'saiu pq não achou nenhum erro'
@@ -94,47 +95,51 @@ class Descritor:
 	# gera o vetor de caracteristica
 	def gerar_vetor(self):
 		local = os.listdir(self.arquivo)
+		random.shuffle(local)
 		for im_file in local:
 			im_path = self.arquivo + '/' + im_file
 			im = cv2.imread(im_path, 0)
 
 			# redimensiona a imagem
-			im = imutils.resize(im, width=80, height=80)
+			im = imutils.resize(im, width=65, height=65)
 
-			# converte a imagem em um vetor
-			vetor_cara = np.asarray(im, dtype='uint8')
+			h, w = im.shape[:2]
 
-			# o resultador é um vetor de 80 posicoes, onde cada posição é um outro vetor com 80 valores
-			# concatena todos os vetores em um vetor só
-			vetor_aux = np.array(vetor_cara).ravel()
+			if h == 65 and w == 65:
+				# converte a imagem em um vetor
+				vetor_cara = np.asarray(im, dtype='uint8')
 
-			# Realizar a normalização dos dados
-			tam = len(vetor_aux)
-			vetor_aux_2 = []
-			for i in range(tam):
-				val = vetor_aux[i] - min(vetor_aux)
-				val2 = max(vetor_aux) - min(vetor_aux)
-				if val2 == 0:
-					vetor_aux_2.append(0)
-				else:
-					vetor_aux_2.append(val/float(val2))
+				# o resultador é um vetor de 80 posicoes, onde cada posição é um outro vetor com 80 valores
+				# concatena todos os vetores em um vetor só
+				vetor_aux = np.array(vetor_cara).ravel()
 
-			string_aux = im_file.split('.')[0]
-			if 'nod' == string_aux:
-				cont = 1
-			elif 'nnod' == string_aux:
-				cont = 0
+				# Realizar a normalização dos dados
+				tam = len(vetor_aux)
+				vetor_aux_2 = []
+				for i in range(tam):
+					val = vetor_aux[i] - min(vetor_aux)
+					val2 = max(vetor_aux) - min(vetor_aux)
+					if val2 == 0:
+						vetor_aux_2.append(0)
+					else:
+						vetor_aux_2.append(val/float(val2))
 
-			self.saidas_esperadas.append(cont)
-			self.vetor_caracteristica.append(vetor_aux_2)
+				string_aux = im_file.split('.')[0]
 
+				if not all([v == 0 for v in vetor_aux_2]) and not all([v == 1 for v in vetor_aux_2]):
+					if 'nod' == string_aux:
+						cont = 1
+					elif 'nnod' == string_aux:
+						cont = 0
+					self.saidas_esperadas.append(cont)
+					self.vetor_caracteristica.append(vetor_aux_2)
 		return self.vetor_caracteristica, self.saidas_esperadas
 
 # ---------------------------------------------------------------------------------------------------
 # MAIN:
 
 # Local com os arquivos:
-string_path = "5_fold/"
+string_path = "folds/"
 accuracy_array = list()
 
 # Posição do folder para teste:
@@ -165,14 +170,12 @@ for i in range(5):
 			# Insere os dados obtidos anteriormente:
 			perceptronControlador.entradas = entradas
 			perceptronControlador.total_entradas = len(entradas)
+			#perceptronControlador.total_variaveis_entrada = len(entradas[0])
 			perceptronControlador.saidas = saidas
 
 			print 'treinar...'
 			# Inicia o processo de treinamento:
 			perceptronControlador.treinar()
-
-			print 'pesos... '
-			print perceptronControlador.pesos
 
 	print 'testar...'
 	# Passa o diretorio que está os dados de teste:
